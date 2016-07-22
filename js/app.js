2843ad8ec5dab730c9ca54e1d19d4a96f51e1ab7
Ryan Rotz <ryanrotz@gmail.com>
2016-07-22 13:37:37 -0700

    fruits and veggies move

--- js/app.js
+++ js/app.js
@@ -1,5 +1,60 @@
 /* setup your angular app here */
 
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', ['$scope', function($scope) {
+  // $scope.fruitsArray = fruits;
+  // $scope.vegetablesArray = vegetables;
+  $scope.foodArray = fruits.concat(vegetables);
+
+  $scope.fruitsArray = [];
+  $scope.vegetablesArray = [];
+
+ $scope.moveLeft = function(idx) {
+  $scope.fruitsArray.push($scope.foodArray[idx]);
+  $scope.foodArray.splice(idx, 1);
+ };
+
+  $scope.moveRight = function(idx) {
+    $scope.vegetablesArray.push($scope.foodArray[idx]);
+    $scope.foodArray.splice(idx, 1);
+  };
+
+  $scope.backLeft = function(idx) {
+    $scope.foodArray.push($scope.vegetablesArray[idx]);
+    $scope.vegetablesArray.splice(idx, 1);
+  };
+
+  $scope.backRight = function(idx) {
+    $scope.foodArray.push($scope.fruitsArray[idx]);
+    $scope.fruitsArray.splice(idx, 1);
+  };
+
+
+}]);
+
+
+// Switched from directives to having everything in the controller
+
+// app.directive('leftArrow', function() {
+//   return {
+//     restrict: 'AE',
+//     replace: true,
+//     templateUrl: 'templates/leftArrow.html'
+//   }
+// })
+
+// app.directive('rightArrow', function() {
+//   return {
+//     restrict: 'AE',
+//     replace: true,
+//     templateUrl: 'templates/rightArrow.html'
+//   }
+// })
+
+
+
+
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
 console.log('Fruit count', fruits.length);
